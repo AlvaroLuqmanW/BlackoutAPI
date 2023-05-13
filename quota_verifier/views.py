b67404d17ed51f342223221bc5e042209b60132f
alvaro.luqman1 <alvaro.luqman@gmail.com>
2023-05-14 02:29:42 +0700

    test commit

--- quota_verifier/views.py
+++ quota_verifier/views.py
@@ -9,16 +9,18 @@ def verifyQuota(request, event, userID, channelID, messageID):
     divisionalEventChannelIDs = ['780627292434726922', '923455236809588778', '736609429167931463']
     contractedEventChannelIDs = ['780623012390633492', '923455236809588778', '736609429167931463', '780623059009798144']
 
-    if event == 'divisional' and channelID not in divisionalEventChannelIDs:
-        return JsonResponse({'message': 'Invalid\nInvalid channel ID'})
-    if event == 'contracted' and channelID not in contractedEventChannelIDs:
-        return JsonResponse({'message': 'Invalid\nInvalid channel ID'})
+    # if event == 'divisional' and channelID not in divisionalEventChannelIDs:
+    #     return JsonResponse({'message': 'Invalid\nInvalid channel ID'})
+    # if event == 'contracted' and channelID not in contractedEventChannelIDs:
+    #     return JsonResponse({'message': 'Invalid\nInvalid channel ID'})
     
     bot_token = BOT_TOKEN
     api_endpoint = f"https://discord.com/api/v9/channels/{channelID}/messages/{messageID}"
     headers = {"Authorization": f"Bot {bot_token}"}
     response = requests.get(api_endpoint, headers=headers)
     message_content = response.json().get('content')
+
+    print(message_content)
 
     if message_content is None:
         return JsonResponse({'message': 'Invalid\nInvalid Server ID'})
